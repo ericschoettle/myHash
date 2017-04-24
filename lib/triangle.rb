3da8342a3e98b253bc5cf5844fc4c1ed59ffbed6
Anna Horodetska and Eric Schoettle <anna@>
2017-04-24 09:18:12 -0700

    add type method for triangle class

---
+++ lib/triangle.rb
@@ -0,0 +1,23 @@
+require ('pry')
+class Triangle
+  define_method(:initialize) do |side1, side2, side3|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  define_method(:side1) do
+    @side1
+  end
+  define_method(:type) do
+    if (@side1 + @side2 <= @side3) ||  (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
+      "Not a triangle"
+    elsif (@side1 == @side2) & (@side1 == @side3)
+      "Equilateral"
+    elsif (@side1 == @side2) || (@side1 == @side3) || (@side2 == @side3)
+      "Isosceles"
+    else
+      "Scalene"  
+    end
+  end
+
+end
